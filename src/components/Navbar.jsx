54e05f18d24511a8b6f31e04a6e1147dd1cd766a
Samuel Ndo <samuel.dmb@outlook.fr>
2024-04-09 22:36:56 -0400

    ✅ 💄 Header done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import Logo_aes from '../assets/logo_service_aes_miniature.webp'
+import './styles/Navbar.css'
 
 function Navbar () {
     return(
-        <header>Lorem ipsum dolor sit amet.</header>
+        <header className='Header'>
+            <img src={Logo_aes} alt="Logo Service AES Dépannage" />
+            <div className="mesinfos">
+               <a href="tel:+33695150779">
+                <i className="fa-solid fa-phone"></i>
+               </a>
+               <a href="mailto:">
+                <i className="fa-solid fa-envelope"></i>
+               </a>
+            </div>
+        </header>
     )
 }
 export default Navbar
