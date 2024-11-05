65fb12ba678de162cb698032044364f69bf10da9
Samuel Ndo <samuel.dmb@outlook.fr>
2024-11-04 20:38:30 -0500

    ✅ 💄 🚧 Services Component done

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,13 +6,19 @@ function Services () {
             <h1>Services</h1>
             <section className='Noservices'>
             <article>
+                <span className="repair"><i className="fa-solid fa-screwdriver-wrench"></i></span>
                 <h2>Dépannage</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita perspiciatis consequatur molestias illum a corrupti eius ex iste facilis accusantium, architecto ratione, sunt sit aliquam numquam aut laudantium. Magni dolorum fugiat, labore beatae dolorem autem. Vero quaerat odio dolorem ad quas molestiae iste maiores doloribus voluptatem perspiciatis. Repellat, quod vitae!</p>
             </article>
             <article>
+                <span className="trucktow"><i className="fa-solid fa-truck-front"></i></span>
                 <h2>Remorquage</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita perspiciatis consequatur molestias illum a corrupti eius ex iste facilis accusantium, architecto ratione, sunt sit aliquam numquam aut laudantium. Magni dolorum fugiat, labore beatae dolorem autem. Vero quaerat odio dolorem ad quas molestiae iste maiores doloribus voluptatem perspiciatis. Repellat, quod vitae!</p>
             </article>
             <article>
+                <span className="transport"><i className="fa-solid fa-road"></i></span>
                 <h2>Transport</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita perspiciatis consequatur molestias illum a corrupti eius ex iste facilis accusantium, architecto ratione, sunt sit aliquam numquam aut laudantium. Magni dolorum fugiat, labore beatae dolorem autem. Vero quaerat odio dolorem ad quas molestiae iste maiores doloribus voluptatem perspiciatis. Repellat, quod vitae!</p>
             </article>
             </section>
         </div>
