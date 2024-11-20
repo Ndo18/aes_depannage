b0512d3b4736541de17d13b45faf5d2a8da7b63e
Samuel Ndo <samuel.dmb@outlook.fr>
2024-11-19 19:25:20 -0500

    💄 🚧 Apropos page done

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
 import './styles/Window.css'
 
 function Window() {
     return(
-        <div className="windowopen"></div>
+        <div className="windowopen">
+            <div className="windowinfo">
+                <p>Dépannage 24/7</p>
+                <p>Sur toute l'Île de France</p>
+            </div>
+        </div>
     )
 }
 export default Window
--- src/components/pages/Apropos.jsx
+++ src/components/pages/Apropos.jsx
@@ -1,7 +1,20 @@
+import '../styles/Apropos.css'
+
 function Apropos() {
     return(
-        <div>
-            <h1>A propos</h1>
+        <div className="Apropos">
+            <h1 id='apropostitle'>À propos</h1>
+        <section className="textapropos">
+            <article>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At doloribus voluptatem, nostrum perspiciatis culpa maiores expedita, repellendus vero voluptates molestiae debitis est quisquam suscipit atque aspernatur veniam dolores eligendi similique? Corrupti doloremque at, rem voluptatem enim quibusdam optio aliquam, dignissimos ipsum inventore iure unde temporibus harum culpa ipsam necessitatibus hic modi non earum? Deserunt, doloremque consectetur. Repellat et beatae eaque.</p>
+            </article><hr />
+            <article>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum atque laboriosam ab architecto nam ea aperiam nisi mollitia eveniet? Ullam esse sunt nesciunt sint, ipsam blanditiis ipsum cupiditate laudantium animi numquam suscipit unde enim cum sequi, minus nobis, doloribus nulla dolores nostrum! Quidem voluptate, quibusdam omnis dolorum delectus atque nobis laboriosam corrupti culpa aliquam at, magni laudantium molestias error blanditiis.</p>
+            </article><hr />
+            <article>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, ea sapiente? Quam unde mollitia ut culpa natus eius perferendis excepturi architecto, sunt eos fugiat dignissimos possimus doloremque tempora quo labore enim provident officiis, soluta similique nam illum ab. Recusandae blanditiis hic tempora optio esse iusto perspiciatis iste ex eos, eligendi dignissimos maxime inventore magni harum quod aut placeat aliquid laboriosam.</p>
+            </article>
+        </section>
         </div>
     )
     
