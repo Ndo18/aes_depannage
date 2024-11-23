c21bd9800621c04552d8405c5e2752d8031bd343
Samuel Ndo <samuel.dmb@outlook.fr>
2024-11-23 01:31:22 -0500

    ✅ Mobile version done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,11 +14,24 @@ function Footer(){
 
     return(
         <footer className="footer">
-            <div className="lienReseaux">
-                {/* <a href="https://github.com/Ndo18"><i className="fa-brands fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/samuel-doumbe-lobe-a3a340151/"><i className="fa-brands fa-linkedin"></i></a> */}
+            <div className="infosutiles">
+                <h2 className="infosutilestitle">Infos utiles</h2>
+                <ul>
+                    <li>Mentions légales & Confidentialité</li>
+                    <li>Dépannage & remorquage VL</li>
+                    <li>Dépannage & remorquage PL</li>
+                    <li>Transpot</li>
+                    <li>Fourrière</li>
+                </ul>
+                <h2 className="suiveznoustitle">Suivez-nous</h2>
+                <div className="liensreseaux">
+                    <a href=""><i className="fa-brands fa-instagram"></i></a>
+                    <a href=""><i className="fa-brands fa-linkedin"></i></a>
+                    <a href=""><i className="fa-brands fa-facebook"></i></a>
+                </div>
+                <hr />
             </div>
-            <div className="copyright"><i className="fa-regular fa-copyright"></i> {currentYear} Le contenu de ce site est protégé par les droits d'auteur et demeure la propriété de Service A.E.S Dépannage, Tous droits réservés.</div>
+            <div className="copyright"><p><i className="fa-regular fa-copyright"></i> {currentYear} Le contenu de ce site est protégé par les droits d'auteur et demeure la propriété de Service A.E.S Dépannage, Tous droits réservés.</p></div>
         </footer>
     )
 }
