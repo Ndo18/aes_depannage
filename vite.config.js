00def52a5d7fcef2160a0e719ede742b1fd1e9d1
Samuel Ndo <samuel.dmb@outlook.fr>
2024-12-05 22:56:41 -0500

    💄 fixed scss

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/aes_depannage/',
   plugins: [react()],
 })
+
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   base: '/nom-de-votre-repository/', // Remplacez par le nom de votre dépôt GitHub
+// })
