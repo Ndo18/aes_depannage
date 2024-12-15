961bd24f74ebd093bfc71618308edf69ad1621e8
Samuel Ndo <samuel.dmb@outlook.fr>
2024-12-15 05:53:55 -0500

    🐛 Fixed services img

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,21 +8,21 @@ function Services () {
             <article className='repairart'>
                 <span className="repair"><i className="fa-solid fa-screwdriver-wrench"></i></span>
                 <h2>Dépannage</h2>
-                <img src="public/photodepannageart2.webp" alt="photo de depannage" loading='lazy' />
+                <img src={`${import.meta.env.BASE_URL}/photodepannageart2.webp"`} alt="photo de depannage" loading='lazy' />
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta sint natus enim iure saepe quod ipsum provident blanditiis fuga consequuntur.</p>
                 <button>En savoir plus</button>
             </article>
             <article className='trucktowart'>
                 <span className="trucktow"><i className="fa-solid fa-truck-front"></i></span>
                 <h2>Remorquage</h2>
-                <img src="public/photoremorquageart.webp" alt="photo remorquage" loading='lazy' />
+                <img src={`${import.meta.env.BASE_URL}/photoremorquageart.webp`} alt="Photo remorquage" loading='lazy'/>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sunt, iusto iure vitae recusandae odio delectus autem vel nulla saepe.</p>
                 <button>En savoir plus</button>
             </article>
             <article className='transportart'>
                 <span className="transport"><i className="fa-solid fa-road"></i></span>
                 <h2>Transport</h2>
-                <img src="public/phototransport.webp" alt="photo de transport de véhicule" loading="lazy" />
+                <img src={`${import.meta.env.BASE_URL}/phototransport.webp`} alt="photo de transport de véhicule" loading="lazy" />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil necessitatibus veritatis sed autem? Amet doloremque ullam ipsa praesentium debitis quibusdam!</p>
                 <button>En savoir plus</button>
             </article>
