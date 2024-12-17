6fb6ae49a2ebe01c64c7e639616856c5d32923bc
Samuel Ndo <samuel.dmb@outlook.fr>
2024-12-16 23:17:13 -0500

    📝 Text apropos + services added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import './styles/Services.css'
 
 function Services () {
@@ -9,21 +10,21 @@ function Services () {
                 <span className="repair"><i className="fa-solid fa-screwdriver-wrench"></i></span>
                 <h2>Dépannage</h2>
                 <img src={`${import.meta.env.BASE_URL}/photodepannageart2.webp`} alt="photo de depannage" loading='lazy' />
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta sint natus enim iure saepe quod ipsum provident blanditiis fuga consequuntur.</p>
+                <p>Notre service de dépannage de véhicules sur place intervient rapidement pour résoudre vos pannes. Batterie déchargée, pneu crevé, problème moteur ou perte de clés, nos techniciens sont là pour vous dépanner directement sur le lieu de l’incident et vous permettre de reprendre la route rapidement.</p>
                 <button>En savoir plus</button>
             </article>
             <article className='trucktowart'>
                 <span className="trucktow"><i className="fa-solid fa-truck-front"></i></span>
                 <h2>Remorquage</h2>
                 <img src={`${import.meta.env.BASE_URL}/photoremorquageart.webp`} alt="Photo remorquage" loading='lazy'/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sunt, iusto iure vitae recusandae odio delectus autem vel nulla saepe.</p>
+                <p>En cas de panne ou d'accident, notre service de remorquage intervient rapidement, que ce soit sur la route, dans un parking ou sur un terrain privé. Disponibles de jour comme de nuit, nos techniciens qualifiés assurent un transport sécurisé de votre véhicule vers le garage de votre choix. Avec nous, gérez la situation en toute sérénité.</p>
                 <button>En savoir plus</button>
             </article>
             <article className='transportart'>
                 <span className="transport"><i className="fa-solid fa-road"></i></span>
                 <h2>Transport</h2>
                 <img src={`${import.meta.env.BASE_URL}/phototransport.webp`} alt="photo de transport de véhicule" loading="lazy" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil necessitatibus veritatis sed autem? Amet doloremque ullam ipsa praesentium debitis quibusdam!</p>
+                <p>Que ce soit pour une voiture neuve ou immobilisée, notre service de transport répond à vos besoins, en France comme à travers l’Europe. Nous intervenons rapidement, quel que soit le point de départ : garage, parking ou tout autre lieu. Votre véhicule est entre de bonnes mains, grâce à un acheminement sûr et sur mesure.</p>
                 <button>En savoir plus</button>
             </article>
             </section>
--- src/components/pages/Apropos.jsx
+++ src/components/pages/Apropos.jsx
@@ -1,20 +1,36 @@
+/* eslint-disable no-irregular-whitespace */
 import '../styles/Apropos.css'
 
 function Apropos() {
     return(
         <div className="Apropos">
             <h1 id='apropostitle'>À propos</h1>
-        <section className="textapropos">
-            <article>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At doloribus voluptatem, nostrum perspiciatis culpa maiores expedita, repellendus vero voluptates molestiae debitis est quisquam suscipit atque aspernatur veniam dolores eligendi similique? Corrupti doloremque at, rem voluptatem enim quibusdam optio aliquam, dignissimos ipsum inventore iure unde temporibus harum culpa ipsam necessitatibus hic modi non earum? Deserunt, doloremque consectetur. Repellat et beatae eaque.</p>
-            </article><hr />
-            <article>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum atque laboriosam ab architecto nam ea aperiam nisi mollitia eveniet? Ullam esse sunt nesciunt sint, ipsam blanditiis ipsum cupiditate laudantium animi numquam suscipit unde enim cum sequi, minus nobis, doloribus nulla dolores nostrum! Quidem voluptate, quibusdam omnis dolorum delectus atque nobis laboriosam corrupti culpa aliquam at, magni laudantium molestias error blanditiis.</p>
-            </article><hr />
-            <article>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, ea sapiente? Quam unde mollitia ut culpa natus eius perferendis excepturi architecto, sunt eos fugiat dignissimos possimus doloremque tempora quo labore enim provident officiis, soluta similique nam illum ab. Recusandae blanditiis hic tempora optio esse iusto perspiciatis iste ex eos, eligendi dignissimos maxime inventore magni harum quod aut placeat aliquid laboriosam.</p>
-            </article>
-        </section>
+<section className="textapropos">
+    <section>
+        <h2>Notre engagement</h2>
+        <p>
+            Chez AES Dépannage, nous plaçons votre sérénité et votre satisfaction au cœur de nos priorités !  
+            Avec des années d’expérience dans le remorquage, le dépannage, et le transport de véhicules, nous sommes fiers d’offrir des services rapides et fiables partout en région et au-delà.  
+            Notre équipe de professionnels qualifiés est dévouée à vous accompagner dans toutes les situations: accident, panne mécanique, ou simple besoin de transport de véhicule en toute sécurité.
+        </p>
+    </section>
+    <hr />
+    <section>
+        <h2>Nos solutions</h2>
+        <p>
+            Que vous soyez bloqué sur une route, dans un parking, ou que vous ayez besoin d’aide pour déplacer un véhicule utilitaire ou un objet encombrant, nous avons la solution !  
+            Grâce à notre équipement moderne et à notre respect des normes les plus strictes, nous garantissons une intervention sécurisée et adaptée à vos besoins.
+        </p>
+    </section>
+    <hr />
+    <section>
+        <h2>Contactez-nous</h2>
+        <p>
+            Contactez-nous dès maintenant pour une estimation gratuite ou une assistance immédiate.  
+            Nous sommes disponibles 24h/24 et 7j/7 pour répondre à vos demandes et vous remettre sur la route en toute tranquillité.
+        </p>
+    </section>
+</section>
         </div>
     )
     
